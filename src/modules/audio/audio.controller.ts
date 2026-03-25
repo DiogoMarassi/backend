b792d97ce3cc58be21c3b8cebf82b9d0bdfffdfc
DiogoMarassi <dmaraassi@gmail.com>
2026-03-24 22:05:23 -0300

    fix: corrigindo erro de tipo

--- src/modules/audio/audio.controller.ts
+++ src/modules/audio/audio.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
-import { Response } from 'express';
+import type { Response } from 'express';
 import { Storage } from '@google-cloud/storage';
 
 @Controller('audio')
@@ -8,7 +8,7 @@ export class AudioController {
 
   @Get(':filename')
   async streamAudio(@Param('filename') filename: string, @Res() res: Response) {
-    const bucketName = process.env.GCS_BUCKET_NAME;
+    const bucketName = process.env.GCS_BUCKET_NAME ?? '';
     const file = this.storage.bucket(bucketName).file(`audios/${filename}`);
     res.setHeader('Content-Type', 'audio/wav');
     res.setHeader('Accept-Ranges', 'bytes');
